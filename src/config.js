76ab5869dafcc57fc3604854f72aaa1d7d39427b
Feliandres <85601974+Feliandres@users.noreply.github.com>
2023-03-24 05:31:13 -0500

    as
    asdaaaa

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const PORT  = process.env.PORT || 3000
 
-export const DB_HOST = process.env.DB_HOST || 'localhost'
-export const DB_USERNAME = process.env.DB_USERNAME || 'root'
-export const DB_PASSWORD = process.env.DB_PASSWORD || ''
-export const DB_DATABSE = process.env.DB_DATABASE || 'prueba'
-export const DB_PORT = process.env.DB_PORT || '3308'
+export const DB_HOST = process.env.DB_HOST //|| 'localhost'
+export const DB_USERNAME = process.env.DB_USERNAME //|| 'root'
+export const DB_PASSWORD = process.env.DB_PASSWORD //|| ''
+export const DB_DATABSE = process.env.DB_DATABASE //|| 'prueba'
+export const DB_PORT = process.env.DB_PORT //|| '3308'
